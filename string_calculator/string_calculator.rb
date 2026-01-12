ec873c3536bcdee4f10c2529673e45ccc8314055
nikhilknick <narya@kyruushealth.com>
2026-01-12 15:05:39 +0530

    Fix failing test cases

--- string_calculator/string_calculator.rb
+++ string_calculator/string_calculator.rb
@@ -1,5 +1,12 @@
 class StringCalculator
-  def add(numbers)
-    return 0 if numbers.empty?
-  end
+    def add(input)
+        return 0 if input.empty?
+      
+        numbers = input.split(/,|\n/).map(&:to_i)
+      
+        negatives = numbers.select { |n| n < 0 }
+        raise "Negatives not allowed: #{negatives.join(', ')}" if negatives.any?
+      
+        numbers.sum
+    end
 end
